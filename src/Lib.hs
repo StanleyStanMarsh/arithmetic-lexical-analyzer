0e2da25c04258cc4c156776c543cfd80bc7a33bf
StanleyStanMarsh <iastafev56@gmail.com>
2024-12-03 00:43:46 +0300

    feat: add instances of Functor and Applicative, binary parser

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,6 +8,32 @@ import Data.Char
 
 newtype Parser a = Parser { runParser :: Text -> Maybe (Text, a) }
 
+instance Functor Parser where
+    fmap :: (a -> b) -> Parser a -> Parser b
+    -- хотим применить func над результатом парсера p
+    fmap func (Parser p) = Parser f where
+        -- парсер f возвращает:
+        f origText = case p origText of
+            Nothing -> Nothing -- Nothing, если парсер p возвращает Nothing
+            Just (remainingP, resP) -> Just (remainingP, func resP) -- (остаток, resP обработанный функцией func), если p возвращает (остаток, resP)
+
+instance Applicative Parser where
+    pure :: a -> Parser a
+    -- возвращаем всегда (изначальная строка, передаваемое значение)
+    pure text = Parser (\orig -> Just(orig, text))
+
+    (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+    -- хотим чтобы был парсер, который применяет к остатку 1 парсера 2 парсер,
+    -- а затем применяет 1 парсер ко 2
+    (Parser u) <*> (Parser v) = Parser f where
+        f origText = case u origText of
+            Nothing -> Nothing
+            -- remainingU - остаток 1 парсера
+            Just (remainingU, resU) -> case v remainingU of
+                Nothing -> Nothing
+                -- remainingV - итоговый остаток, resU применяем над resV
+                Just (remainingV, resV) -> Just (remainingV, resU resV)
+
 isBinary :: Text -> Bool
 isBinary = T.all (`elem` ("01" :: String))
 
@@ -16,3 +42,5 @@ satisfy pr = Parser f where
     f cs | pr cs = Just (T.pack "", cs)
     f _ = Nothing
 
+binary :: Parser Text
+binary = satisfy isBinary
