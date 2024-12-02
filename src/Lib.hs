92d81a23d7f96e4fb7e768f47458ff741cc3dac5
StanleyStanMarsh <iastafev56@gmail.com>
2024-12-02 23:13:40 +0300

    feat, fix: add Parser type instead of function, add satisfy function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,18 @@
 module Lib
     ( isBinary
-    , findBinaries
     ) where
 
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Char
 
+newtype Parser a = Parser { runParser :: Text -> Maybe (Text, a) }
+
 isBinary :: Text -> Bool
 isBinary = T.all (`elem` ("01" :: String))
 
-findBinaries :: Text -> [Text]
-findBinaries input = filter isBinary $ T.words $ T.map replaceNonBinary input
-  where
-    replaceNonBinary c
-      | isDigit c = c
-      | otherwise = ' '
+satisfy :: (Text -> Bool) -> Parser Text
+satisfy pr = Parser f where
+    f cs | pr cs = Just (T.pack "", cs)
+    f _ = Nothing
+
