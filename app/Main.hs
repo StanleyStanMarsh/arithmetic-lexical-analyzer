65b2351d45431f94815f3ae277896a48bf968b61
StanleyStanMarsh <iastafev56@gmail.com>
2024-12-02 18:17:34 +0300

    feat: add binaries parser function

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
 import Lib
+import Data.Text (pack)
 
 main :: IO ()
-main = 
+main = do
+    print $ findBinaries $ pack "qasldjnasdf 123 asdknlasd 0001010 askdjfn 000011111"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,18 @@
 module Lib
     ( isBinary
+    , findBinaries
     ) where
 
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Char
 
 isBinary :: Text -> Bool
 isBinary = T.all (`elem` ("01" :: String))
+
+findBinaries :: Text -> [Text]
+findBinaries input = filter isBinary $ T.words $ T.map replaceNonBinary input
+  where
+    replaceNonBinary c
+      | isDigit c = c
+      | otherwise = ' '
