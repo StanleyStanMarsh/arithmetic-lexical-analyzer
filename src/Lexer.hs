dc55c7d0b7b756c7b199eb2385075566bc7e0ab6
StanleyStanMarsh <iastafev56@gmail.com>
2025-04-02 21:33:50 +0300

    feat: add key words

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -2,17 +2,20 @@ module Lexer
     ( lexer
     ) where
 
-import Data.Char (isDigit, isAlpha, isAlphaNum)
+import Data.Char (isAlpha, isAlphaNum, isDigit)
 
+-- Определение токенов
 data Token = Identifier String
            | Number String
            | Assign
            | Plus | Minus | Mul | Div
            | LParen | RParen
            | Semicolon
+           | Begin | End
            | Error String
            deriving (Show, Eq)
 
+-- Основная функция лексического анализа
 lexer :: String -> [Token]
 lexer [] = []
 lexer (' ':xs) = lexer xs
@@ -27,25 +30,40 @@ lexer ('/':xs) = Div : lexer xs
 lexer ('(':xs) = LParen : lexer xs
 lexer (')':xs) = RParen : lexer xs
 lexer (':':'=':xs) = Assign : lexer xs
-lexer (x:xs) | isAlpha x = lexIdentifier (x:xs)
+lexer (x:xs) | isAlpha x = lexKeywordOrIdentifier (x:xs)
              | isDigit x = lexNumber (x:xs)
              | otherwise = Error [x] : lexer xs
 
-lexIdentifier :: String -> [Token]
-lexIdentifier xs = let (ident, rest) = span isAlphaNum xs
-                   in if length ident > 16
-                      then Error "Identifier too long" : lexer rest
-                      else Identifier ident : lexer rest
+-- Обработка ключевых слов и идентификаторов
+lexKeywordOrIdentifier :: String -> [Token]
+lexKeywordOrIdentifier xs
+  | isKeyWord ident = keywordToken ident : lexer rest
+  | otherwise = Identifier ident : lexer rest
+  where
+    (ident, rest) = span isAlphaNum xs
 
+-- Проверка на ключевое слово
+isKeyWord :: String -> Bool
+isKeyWord x = x == "begin" || x == "end"
+
+-- Возвращает соответствующий токен для ключевых слов
+keywordToken :: String -> Token
+keywordToken "begin" = Begin
+keywordToken "end" = End
+keywordToken _ = Error "Unknown keyword"
+
+-- Обработка чисел
 lexNumber :: String -> [Token]
 lexNumber xs = let (num, rest) = span (`elem` ".0123456789eE-") xs
                in Number num : lexer rest
 
+-- Функция для удаления комментариев
 dropComment :: String -> String
 dropComment ('*':'/':xs) = xs
 dropComment (_:xs) = dropComment xs
 dropComment [] = [] -- незакрытый комментарий, но игнорируем
 
-testInput = "    x := (3+4j) * y; //комментарии могут быть любой длинны 123456789098765432123456789 комекрп\n\
-    \y := 5.6 - 7.8j;\
-    \x := x ^ 2;"
+testInput = "    begin\n\
+            \x := 3 + 4; // комментарий\n\
+            \end\n"
+
