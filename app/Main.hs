3b7851fb65cfadf1f6119e1e89efb69dcf02a00a
StanleyStanMarsh <iastafev56@gmail.com>
2025-04-04 10:14:52 +0300

    feat: add lexer with test strings

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,21 @@
 module Main (main) where
 
-import Parser
+import Lexer
 import System.IO
-import qualified Data.Text as T
 
 main :: IO ()
 main = do
-    let input = "x := 12.34 + y - 5.67e-2; # This is a comment"
-    case Parser.lexer (T.pack input) of
-        Left err -> putStrLn ("Error: " ++ T.unpack err)
-        Right tokens -> print tokens
+    let testInput1 = "begin\n\
+                     \x := 3 + 4; # комментарий\n\
+                     \y := 5.6 -@!$ 7.8j;;;\n\
+                     \end"
+    let testInput2 = "x := 12.34 + y - 5.67e-2; # This is a comment"
+    let testInput3 = "begin\n\
+                     \1.2.3;\n\
+                     \ident2 := -105;"
+    putStrLn "input 1:"
+    printLexTable testInput1
+    putStrLn "input 2:"
+    printLexTable testInput2
+    putStrLn "input 3:"
+    printLexTable testInput3
--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,12 +1,14 @@
-module Lexer
-    ( lexer
+module Lexer 
+    ( lexer,
+      printLexTable
     ) where
 
-import Data.Char (isAlpha, isAlphaNum, isDigit)
+import Data.Char (isAlpha, isAlphaNum, isDigit, isSpace)
 
 -- Определение токенов
-data Token = Identifier String
-           | Number String
+data Token = Identifier String Int
+           | IntNumber Int
+           | FloatNumber Float
            | Assign
            | Plus | Minus | Mul | Div
            | LParen | RParen
@@ -15,36 +17,64 @@ data Token = Identifier String
            | Error String
            deriving (Show, Eq)
 
+-- Маппинг ключевых слов на уникальные значения (X1, X2, ...)
+keywordMapping :: [(String, String)]
+keywordMapping = [("begin", "X1"), ("end", "X2")]
+
 -- Основная функция лексического анализа
 lexer :: String -> [Token]
 lexer [] = []
-lexer (' ':xs) = lexer xs
-lexer ('\n':xs) = lexer xs
-lexer (';':xs) = Semicolon : lexer xs
-lexer ('+':xs) = Plus : lexer xs
-lexer ('-':xs) = Minus : lexer xs
-lexer ('*':xs) = Mul : lexer xs
-lexer ('/':'/':xs) = lexer (dropWhile (/='\n') xs) -- однострочный комментарий
-lexer ('/':'*':xs) = lexer (dropComment xs) -- многострочный комментарий
-lexer ('/':xs) = Div : lexer xs
-lexer ('(':xs) = LParen : lexer xs
-lexer (')':xs) = RParen : lexer xs
-lexer (':':'=':xs) = Assign : lexer xs
-lexer (x:xs) | isAlpha x = lexKeywordOrIdentifier (x:xs)
-             | isDigit x = lexNumber (x:xs)
-             | otherwise = Error [x] : lexer xs
+lexer (c:cs)
+  | isSpace c = lexer cs  -- Пропускаем пробелы и символы новой строки
+  | c == ';'  = lexer cs
+  | c == '#'  = lexer (dropWhile (/='\n') cs)  -- Однострочные комментарии
+  | c == '+'  = Plus : lexer cs
+  | c == '-'  = Minus : lexer cs
+  | c == '*'  = Mul : lexer cs
+  | c == '/'  = Div : lexer cs
+  | c == '('  = LParen : lexer cs
+  | c == ')'  = RParen : lexer cs
+  | c == ':'  = lexerAssign cs
+  | isAlpha c = lexKeywordOrIdentifier (c:cs)
+  | isDigit c = lexNumber (c:cs)
+  | otherwise = Error [c] : lexer cs  -- Ошибка, если символ не распознан
 
--- Обработка ключевых слов и идентификаторов
+identifierCheck :: String -> Token
+identifierCheck x
+    | length x > 16 = Error "Identifier too long"
+    | all (`elem` (['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'])) x = Identifier x 0
+    | otherwise = Error "Identifier contains invalid characters"
+
 lexKeywordOrIdentifier :: String -> [Token]
 lexKeywordOrIdentifier xs
   | isKeyWord ident = keywordToken ident : lexer rest
-  | otherwise = Identifier ident : lexer rest
+  | otherwise = identifierCheck ident : lexer rest
   where
+    -- Идентификатор состоит из букв и цифр, но должен начинаться с буквы
     (ident, rest) = span isAlphaNum xs
+
+-- Обработка присваивания ":="
+lexerAssign :: String -> [Token]
+lexerAssign ('=':xs) = Assign : lexer xs
+lexerAssign _ = Error "Invalid assignment operator" : []
+
+-- Обработка чисел
+lexNumber :: String -> [Token]
+lexNumber xs =
+    let (num, rest) = span isDigit xs
+    in case rest of
+        ('.':ys) -> lexFloat (num ++ ".") ys
+        _        -> IntNumber (read num) : lexer rest
+
+-- Обработка числа с плавающей точкой
+lexFloat :: String -> String -> [Token]
+lexFloat xs ys =
+    let (fracPart, rest) = span isDigit ys
+    in FloatNumber (read (xs ++ fracPart)) : lexer rest
 
 -- Проверка на ключевое слово
 isKeyWord :: String -> Bool
-isKeyWord x = x == "begin" || x == "end"
+isKeyWord x = x `elem` map fst keywordMapping
 
 -- Возвращает соответствующий токен для ключевых слов
 keywordToken :: String -> Token
@@ -52,18 +82,55 @@ keywordToken "begin" = Begin
 keywordToken "end" = End
 keywordToken _ = Error "Unknown keyword"
 
--- Обработка чисел
-lexNumber :: String -> [Token]
-lexNumber xs = let (num, rest) = span (`elem` ".0123456789eE-") xs
-               in Number num : lexer rest
-
 -- Функция для удаления комментариев
 dropComment :: String -> String
 dropComment ('*':'/':xs) = xs
 dropComment (_:xs) = dropComment xs
-dropComment [] = [] -- незакрытый комментарий, но игнорируем
+dropComment [] = [] -- незакрытый комментарий
 
-testInput = "    begin\n\
-            \x := 3 + 4; // комментарий\n\
-            \end\n"
+-- Функция для присваивания уникальных номеров идентификаторам
+assignIdentifierNumbers :: [Token] -> [Token]
+assignIdentifierNumbers tokens = assignNumbers [] 1 tokens
+  where
+    -- Вспомогательная функция для обхода списка токенов
+    assignNumbers _ _ [] = []
+    
+    -- Обработка идентификаторов
+    assignNumbers used count (Identifier name _ : ts)
+      | Just num <- lookup name used =
+          Identifier name num : assignNumbers used count ts
+      | otherwise =
+          let newUsed = (name, count) : used
+          in Identifier name count : assignNumbers newUsed (count + 1) ts
+    
+    -- Пропуск остальных токенов
+    assignNumbers used count (t:ts) = t : assignNumbers used count ts
+
+-- Печать таблицы лексем
+printLexTable :: String -> IO ()
+printLexTable input = do
+    let tokens = lexer input
+    let tokensWithNumbers = assignIdentifierNumbers tokens
+    putStrLn "Таблица лексем:\n"
+    putStrLn $ "Лексема" ++ tabs ++ "Тип лексемы" ++ tabs ++ "Значение"
+    mapM_ printToken tokensWithNumbers
+
+tabs :: String
+tabs = "\t\t\t\t"
+
+-- Печать отдельного токена
+printToken :: Token -> IO ()
+printToken (Identifier name num) = putStrLn $ name ++ tabs ++ "Идентификатор" ++ tabs ++ name ++ " : " ++ show num
+printToken (IntNumber num) = putStrLn $ show num ++ tabs ++ "Целочисленная константа\t\t\t" ++ show num
+printToken (FloatNumber num) = putStrLn $ show num ++ tabs ++ "Вещественная константа\t\t\t" ++ show num
+printToken Assign = putStrLn $ ":=" ++ tabs ++ "Знак присваивания"
+printToken Plus = putStrLn $ "+" ++ tabs ++ "Знак арифметической операции"
+printToken Minus = putStrLn $ "-" ++ tabs ++ "Знак арифметической операции"
+printToken Mul = putStrLn $ "*" ++ tabs ++ "Знак арифметической операции"
+printToken Div = putStrLn $ "/" ++ tabs ++ "Знак арифметической операции"
+printToken LParen = putStrLn $ "(" ++ tabs ++ "Знак операции"
+printToken RParen = putStrLn $ ")" ++ tabs ++ "Знак операции"
+printToken Begin = putStrLn $ "begin" ++ tabs ++ "Ключевое слово" ++ tabs ++ "X1"
+printToken End = putStrLn $ "end" ++ tabs ++ "Ключевое слово" ++ tabs ++ "X2"
+printToken (Error msg) = putStrLn $ "Ошибка" ++ tabs ++ msg
 
