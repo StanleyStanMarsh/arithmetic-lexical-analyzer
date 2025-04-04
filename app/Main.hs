39d491d94f9eaf107407a0d71b82164f17c04c00
StanleyStanMarsh <iastafev56@gmail.com>
2025-04-04 11:14:12 +0300

    feat, fix: add menu, improve exponent float form

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,41 @@ module Main (main) where
 
 import Lexer
 import System.IO
+import Control.Monad (unless)
 
 main :: IO ()
-main = do
-    let testInput1 = "begin\n\
-                     \x := 3 + 4; # комментарий\n\
-                     \y := 5.6 -@!$ 7.8j;;;\n\
-                     \end"
-    let testInput2 = "x := 12.34 + y - 5.67e-2; # This is a comment"
-    let testInput3 = "begin\n\
-                     \1.2.3;\n\
-                     \ident2 := -105;"
-    putStrLn "input 1:"
-    printLexTable testInput1
-    putStrLn "input 2:"
-    printLexTable testInput2
-    putStrLn "input 3:"
-    printLexTable testInput3
+main = loop
+  where
+    loop = do
+        putStrLn "Выберите номер строки (1-4) или введите 0 для выхода\n"
+        putStrLn $ "1)\n" ++ testInput1 ++ "\n"
+        putStrLn $ "2)\n" ++ testInput2 ++ "\n"
+        putStrLn $ "3)\n" ++ testInput3 ++ "\n"
+        putStrLn $ "4)\n" ++ testInput4 ++ "\n"
+        putStrLn "Номер строки: "
+        hFlush stdout
+        input <- getLine
+        case input of
+            "1" -> printLexTable testInput1 >> loop
+            "2" -> printLexTable testInput2 >> loop
+            "3" -> printLexTable testInput3 >> loop
+            "4" -> printLexTable testInput4 >> loop
+            "0" -> putStrLn "Выход из программы."
+            _   -> putStrLn "Некорректный ввод, попробуйте снова." >> loop
+
+testInput1 :: String
+testInput1 = "begin\n\
+             \x := 3 + 4; # comment\n\
+             \y := 5.6 -@!$ 7.8j;;;\n\
+             \end"
+
+testInput2 :: String
+testInput2 = "x := 12.34 + y - 5.67e-2; # extremely-very long comment 123456678901239238492384"
+
+testInput3 :: String
+testInput3 = "begin\n\
+             \1.2.3;\n\
+             \ident2 := -105;"
+
+testInput4 :: String
+testInput4 = "a * (b + c) - a * b - a * c + x / y # all operations"
--- src/Lexer.hs
+++ src/Lexer.hs
@@ -5,7 +5,7 @@ module Lexer
 
 import Data.Char (isAlpha, isAlphaNum, isDigit, isSpace)
 
--- Определение токенов
+-- токены
 data Token = Identifier String Int
            | IntNumber Int
            | FloatNumber Float
@@ -17,17 +17,16 @@ data Token = Identifier String Int
            | Error String
            deriving (Show, Eq)
 
--- Маппинг ключевых слов на уникальные значения (X1, X2, ...)
+-- маппинг ключевых слов на уникальные значения (X1, X2, ...)
 keywordMapping :: [(String, String)]
 keywordMapping = [("begin", "X1"), ("end", "X2")]
 
--- Основная функция лексического анализа
 lexer :: String -> [Token]
 lexer [] = []
 lexer (c:cs)
-  | isSpace c = lexer cs  -- Пропускаем пробелы и символы новой строки
+  | isSpace c = lexer cs
   | c == ';'  = lexer cs
-  | c == '#'  = lexer (dropWhile (/='\n') cs)  -- Однострочные комментарии
+  | c == '#'  = lexer (dropWhile (/='\n') cs)
   | c == '+'  = Plus : lexer cs
   | c == '-'  = Minus : lexer cs
   | c == '*'  = Mul : lexer cs
@@ -50,52 +49,65 @@ lexKeywordOrIdentifier xs
   | isKeyWord ident = keywordToken ident : lexer rest
   | otherwise = identifierCheck ident : lexer rest
   where
-    -- Идентификатор состоит из букв и цифр, но должен начинаться с буквы
+    -- состоит из букв и цифр, но должен начинаться с буквы
     (ident, rest) = span isAlphaNum xs
 
--- Обработка присваивания ":="
-lexerAssign :: String -> [Token]
-lexerAssign ('=':xs) = Assign : lexer xs
-lexerAssign _ = Error "Invalid assignment operator" : []
-
--- Обработка чисел
-lexNumber :: String -> [Token]
-lexNumber xs =
-    let (num, rest) = span isDigit xs
-    in case rest of
-        ('.':ys) -> lexFloat (num ++ ".") ys
-        _        -> IntNumber (read num) : lexer rest
-
--- Обработка числа с плавающей точкой
-lexFloat :: String -> String -> [Token]
-lexFloat xs ys =
-    let (fracPart, rest) = span isDigit ys
-    in FloatNumber (read (xs ++ fracPart)) : lexer rest
-
--- Проверка на ключевое слово
 isKeyWord :: String -> Bool
 isKeyWord x = x `elem` map fst keywordMapping
 
--- Возвращает соответствующий токен для ключевых слов
+-- токен для ключевых слов
 keywordToken :: String -> Token
 keywordToken "begin" = Begin
 keywordToken "end" = End
 keywordToken _ = Error "Unknown keyword"
 
--- Функция для удаления комментариев
-dropComment :: String -> String
-dropComment ('*':'/':xs) = xs
-dropComment (_:xs) = dropComment xs
-dropComment [] = [] -- незакрытый комментарий
+-- присваивание
+lexerAssign :: String -> [Token]
+lexerAssign ('=':xs) = Assign : lexer xs
+lexerAssign _ = Error "Invalid assignment operator" : []
 
--- Функция для присваивания уникальных номеров идентификаторам
+-- числа
+lexNumber :: String -> [Token]
+lexNumber xs =
+    let (num, rest) = span isDigit xs
+    in case rest of
+        ('.':ys) -> lexFloat (num ++ ".") ys  -- если точка, переходим к вещественному
+        ('e':ys) -> lexExponent (num ++ "e") ys  -- если есть экспонента, переходим к обработке экспоненты
+        ('E':ys) -> lexExponent (num ++ "E") ys  -- для заглавной 'E'
+        _        -> IntNumber (read num) : lexer rest  -- целое
+
+-- числа с плавающей точкой
+lexFloat :: String -> String -> [Token]
+lexFloat xs ys =
+    let (fracPart, rest) = span isDigit ys
+    in case rest of
+        ('e':zs) -> lexExponent (xs ++ fracPart ++ "e") zs
+        ('E':zs) -> lexExponent (xs ++ fracPart ++ "E") zs
+        _        -> FloatNumber (read (xs ++ fracPart)) : lexer rest
+
+-- обработка экспоненциальной части
+lexExponent :: String -> String -> [Token]
+lexExponent xs (c:cs)
+    | c == '+' || c == '-' =  -- знак экспоненты (e+2, e-3)
+        let (expDigits, rest) = span isDigit cs
+        in if null expDigits
+           then Error "Invalid exponent format" : lexer rest  -- если после e+ или e- нет цифр
+           else FloatNumber (read (xs ++ c : expDigits)) : lexer rest
+    | isDigit c =
+        let (expDigits, rest) = span isDigit (c:cs)
+        in FloatNumber (read (xs ++ expDigits)) : lexer rest
+    | otherwise = Error "Invalid exponent format" : lexer (c:cs)  -- если e не сопровождается цифрами
+
+lexExponent xs [] = Error "Invalid exponent format" : []  -- если строка обрывается на e
+
+-- присваивание уникальных номеров идентификаторам
 assignIdentifierNumbers :: [Token] -> [Token]
 assignIdentifierNumbers tokens = assignNumbers [] 1 tokens
   where
-    -- Вспомогательная функция для обхода списка токенов
+    -- вспомогательная функция для обхода списка токенов
     assignNumbers _ _ [] = []
     
-    -- Обработка идентификаторов
+    -- обработка идентификаторов
     assignNumbers used count (Identifier name _ : ts)
       | Just num <- lookup name used =
           Identifier name num : assignNumbers used count ts
@@ -103,12 +115,13 @@ assignIdentifierNumbers tokens = assignNumbers [] 1 tokens
           let newUsed = (name, count) : used
           in Identifier name count : assignNumbers newUsed (count + 1) ts
     
-    -- Пропуск остальных токенов
+    -- пропуск остальных токенов
     assignNumbers used count (t:ts) = t : assignNumbers used count ts
 
--- Печать таблицы лексем
+-- печать таблицы лексем
 printLexTable :: String -> IO ()
 printLexTable input = do
+    putStrLn $ input ++ "\n"
     let tokens = lexer input
     let tokensWithNumbers = assignIdentifierNumbers tokens
     putStrLn "Таблица лексем:\n"
@@ -118,7 +131,7 @@ printLexTable input = do
 tabs :: String
 tabs = "\t\t\t\t"
 
--- Печать отдельного токена
+-- печать отдельного токена
 printToken :: Token -> IO ()
 printToken (Identifier name num) = putStrLn $ name ++ tabs ++ "Идентификатор" ++ tabs ++ name ++ " : " ++ show num
 printToken (IntNumber num) = putStrLn $ show num ++ tabs ++ "Целочисленная константа\t\t\t" ++ show num
